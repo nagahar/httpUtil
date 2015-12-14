d79044d3ace44ebe4857d76401595062a5f9cce6
Takanori Nagahara <takanori.nagahara@gmail.com>
2015-12-15 00:39:03 +0900

    add parser.js

--- parser.js
+++ parser.js
@@ -1,51 +1,103 @@
-// value
-function Parser(value, stream) {
-    this.value = value;
-    this.stream = stream;
-}
+Parser = (function() {
+    // value
+    function Parser(value, input) {
+        this.value = value;
+        this.input = input;
+    }
 
-// monadic function
-bind = function(mparser, mfunc) {
-    return function(stream) {
-        var result = mparser(stream);
-        if (result === null) {
-            return null;
-        } else {
-            return mfunc(result.value)(result.stream);
+    // monad
+    Parser.bind = function(monadicV, monadicF) {
+        return function(input) {
+            var result = monadicV(input);
+            if (result === null) {
+                return null;
+            } else {
+                return monadicF(result.value)(result.input);
+            }
         }
     }
-}
+
+    // monadic value
+    Parser.unit = function(value) {
+        return function(input) {
+            return new Parser(value, input);
+        }
+    }
+
+    // monadic value
+    Parser.failure = function(input) {
+        return null;
+    }
+
+    // monadic value
+    Parser.item = function(input) {
+        if (input === null) {
+            return null;
+        } else {
+            return new Parser(input.charAt(0), input.slice(1));
+        }
+    }
+
+    return Parser;
+})();
+
+var p = Parser.bind(Parser.item, function(x) {
+    return Parser.bind(Parser.item, function() {
+        return Parser.bind(Parser.item, function(y) {
+            return Parser.unit([x, y]);
+        });
+    });
+});
+
+var s = p("abcdef");
+console.log(JSON.stringify(s));
+function monad(f) {
+    f.bind = function(g) {
+        var _self = this;
+        return function(input) {
+            var val = _self(input);
+            if (val === null) {
+                return null;
+            } else {
+                return g(val[0])(val[1]);
+            }
+        };
+    };
+
+    return f;
+};
 
 // monadic value
-unit = function(value) {
-    return function(stream) {
-        return new Parser(value, stream);
-    }
-}
-
-failure = function(stream) {
-    return null;
-}
-
-item = function(stream) {
-    if (stream === null) {
+var item = monad(function(input) {
+    if (input === null) {
         return null;
     } else {
-        return new Parser(stream.charAt(0), stream.substr(1));
+        return [input.charAt(0), input.slice(1)];
     }
-}
+});
 
-var p = bind(item, function(x) {
-    return bind(item, function() {
-        return bind(item, function(y) {
+// monadic value
+var unit = monad(function(value) {
+    return function(input) {
+        return [value, input];
+    }
+});
+
+// monadic value
+var failure = monad(function(input) {
+    return null;
+});
+
+var p2 = item.bind(function(x) {
+    return item.bind(function() {
+        return item.bind(function(y) {
             return unit([x, y]);
         });
     });
 });
 
-var r = p("abcdef");
-//var r = parse(item, "");
-//var r = parse(item, "abc");
-console.log(JSON.stringify(r));
+
+var s = p2("abcdef");
+console.log(JSON.stringify(s));
 
 
